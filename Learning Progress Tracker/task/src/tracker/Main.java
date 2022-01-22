8a6dedb3ecf3fece25fd83c6e0f4d5bd8f7741cc
Anthony Grear <tgrear2008@gmail.com>
2022-01-22 14:14:06 +0700

    initial commit

---
+++ Learning Progress Tracker/task/src/tracker/Main.java
@@ -0,0 +1,12 @@
+package tracker;
+
+public class Main {
+
+    static void displayStartProgramMessage() {
+        System.out.println("Learning Progress Tracker");
+    }
+
+    public static void main(String[] args) {
+        displayStartProgramMessage();
+    }
+}

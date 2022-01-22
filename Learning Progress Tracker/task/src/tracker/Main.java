bd9c59ba0ef3360571bec248d560256daaee9f60
Anthony Grear <tgrear2008@gmail.com>
2022-01-22 19:57:51 +0700

    Build state machine with public enum. Check for user input: null, exit, or default (not null or not exit).

--- Learning Progress Tracker/task/src/tracker/Main.java
+++ Learning Progress Tracker/task/src/tracker/Main.java
@@ -1,6 +1,48 @@
 package tracker;
 
+
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    private static boolean exit = false;
+
+    public enum TrackerState {
+
+        START_TRACKER {
+            @Override
+            public TrackerState nextState() {
+                displayStartProgramMessage();
+                return MAIN_MENU;
+            }
+        },
+        MAIN_MENU{
+            @Override
+            public TrackerState nextState() {
+                String input = scanner.nextLine().strip();
+                switch (input) {
+                    case "":
+                        System.out.println("no input");
+                        return MAIN_MENU;
+                    case "exit":
+                        return EXIT_TRACKER;
+                    default:
+                        System.out.println("Error: unknown command!");
+                        return MAIN_MENU;
+                }
+            }
+        },
+        EXIT_TRACKER {
+            @Override
+            public TrackerState nextState() {
+                displayExitProgramMessage();
+                exit = true;
+                return this;
+            }
+        };
+
+        public abstract TrackerState nextState();
+    }
 
     static void displayStartProgramMessage() {
         System.out.println("Learning Progress Tracker");
@@ -11,7 +53,10 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        displayStartProgramMessage();
-        displayExitProgramMessage();
+        TrackerState state = TrackerState.START_TRACKER;
+        while (!exit) {
+            state = state.nextState();
+        }
+
     }
 }
